ca165e1bd2a65bbbdc66aa2d5ba3585cb8bdae38
psachania <prakash.sachania@in.ibm.com@prakashs-macbook-pro15.local>
2017-12-05 19:01:41 -0600

    First version

---
+++ TestingNavigationBackButton/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  TestingNavigationBackButton
+//
+//  Created by Prakash Sachania on 12/5/17.
+//  Copyright © 2017 Prakash Sachania. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindTo2ndController(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
